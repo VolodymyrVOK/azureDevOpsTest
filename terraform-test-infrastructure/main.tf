8393495976224b44dce934c0a9567737848b9751
VolodymyrVOK <55443172+VolodymyrVOK@users.noreply.github.com>
2019-11-06 11:55:30 +0200

    Update main.tf

--- terraform-test-infrastructure/main.tf
+++ terraform-test-infrastructure/main.tf
@@ -1,7 +1,10 @@
+variable "client_secret" {}
+
 provider "azurerm" {
+        version = "=2.4.0"
         subscription_id = "c444b664-6329-4b22-85c0-bf70e86569af"
         client_id       = "ce690fb0-8175-4247-b95b-13fdc9dd8b56"
-        client_secret   = "=iV@rlUBXIxHn/EgR:GlMbk6K34Eh1hb"
+        client_secret   = var.client_secret
         tenant_id       = "9c8176a3-82ef-4e17-b391-373802f3d8d4"
 }
 resource "azurerm_resource_group" "rg" {
